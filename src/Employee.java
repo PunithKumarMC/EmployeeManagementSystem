9028b29345f457d94a527533e0c78f1ed30bf0ab
Punith Kumar MC <113302682+PunithKumarMC@users.noreply.github.com>
2023-05-02 14:34:19 +0530

    added Employee class with attributes, constructor and getters, setters

---
+++ src/Employee.java
@@ -0,0 +1,70 @@
+public class Employee {
+    private int EmployeeId;
+
+    private String Name;
+
+    private String dob;
+
+    private String Email;
+
+    private String Address;
+
+    public Employee(int employeeId, String name, String dob, String email, String address) {
+        EmployeeId = employeeId;
+        Name = name;
+        this.dob = dob;
+        Email = email;
+        Address = address;
+    }
+
+    public int getEmployeeId() {
+        return EmployeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        EmployeeId = employeeId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "EmployeeId=" + EmployeeId +
+                ", Name='" + Name + '\'' +
+                ", dob='" + dob + '\'' +
+                ", Email='" + Email + '\'' +
+                ", Address='" + Address + '\'' +
+                '}';
+    }
+}
