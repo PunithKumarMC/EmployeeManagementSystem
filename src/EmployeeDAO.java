eaa9dc9a3edad460d29e28be987f40e3613ceead
Punith Kumar MC <113302682+PunithKumarMC@users.noreply.github.com>
2023-05-02 14:41:05 +0530

    added all method signatures in EmployeeDao.java class

---
+++ src/EmployeeDAO.java
@@ -0,0 +1,22 @@
+public class EmployeeDAO {
+
+   public void addEmployee(Employee employee){
+
+   }
+
+   public void  getEmployeeByName(String name){
+
+   }
+   public void  getEmployeeByEmail(String email){
+
+   }
+
+   public void  getEmployeeById(int id){
+
+   }
+
+   public void   getAllEmployees(){
+
+   }
+
+}
